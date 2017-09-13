4299357c6d5cfa21eb5c2b7bc72fb53be3ef2646
Billy <billychartanto@gmail.com>
2017-09-13 08:52:19 +0800

    initial commit

---
+++ src/main/java/com/allnewthor/tas/TasApplication.java
@@ -0,0 +1,12 @@
+package com.allnewthor.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasApplication.class, args);
+	}
+}
