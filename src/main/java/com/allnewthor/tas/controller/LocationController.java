12dcaf4f9553fec870356981043f9d0bd7a4118a
Inez Sutanto <InezFiona.Sutanto@mitrais.com>
2017-09-14 11:27:48 +0800

    initial commit

---
+++ src/main/java/com/allnewthor/tas/controller/LocationController.java
@@ -0,0 +1,22 @@
+package com.allnewthor.tas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.allnewthor.tas.domain.Location;
+import com.allnewthor.tas.domain.LocationRepository;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationController {
+	@Autowired
+	private LocationRepository locationRepository;
+	
+	@RequestMapping(value="",method=RequestMethod.GET)
+	public Iterable<Location> getAll(Model model) {
+		return locationRepository.findAll();
+    }
+}
